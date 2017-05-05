5f41d3a487d78000879dbf8d6724c0c731f023dc
ashrafulsiddique <ashrafulsiddique@users.noreply.github.com>
2017-05-05 19:59:43 +0600

    Add files via upload

---
+++ Do_while_loop.java
@@ -0,0 +1,12 @@
+
+public class Do_while_loop {
+	public static void main(String[] args){
+		int counter =0;
+		
+		do{
+			System.out.println(counter);
+		counter++;
+		}while(counter <=10);			
+		
+	}
+}
