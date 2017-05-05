de370c5d9d9aaae1a55eb49e6856e68ffe06760b
ashrafulsiddique <ashrafulsiddique@users.noreply.github.com>
2017-05-05 20:19:59 +0600

    Add files via upload

---
+++ Math_Class_Method.java
@@ -0,0 +1,11 @@
+
+public class Math_Class_Method {
+	public static void main(String[] args){
+		
+		System.out.println(Math.max(8.6,5.2));
+		//System.out.println(Math.floor(7.8));
+		//System.out.println(Math.abs(-26.7));
+		//System.out.println(Math.ceil(7.4));
+		
+	}
+}
