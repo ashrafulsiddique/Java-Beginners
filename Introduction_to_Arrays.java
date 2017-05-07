6ecbd4a0f6c2250acc0336b76b30761b9fc94058
ashrafulsiddique <ashrafulsiddique@users.noreply.github.com>
2017-05-07 20:40:49 +0600

    Add files via upload

---
+++ Introduction_to_Arrays.java
@@ -0,0 +1,15 @@
+
+public class Introduction_to_Arrays {
+	public static void main(String[] args){
+		
+		int ash[]= new int[10];
+		
+		ash[0]=89;
+		ash[1]=432;
+		ash[9]=67;
+		// int ash[]={2,4,5,7};
+		
+		System.out.println(ash[9]);
+		
+	}
+}
