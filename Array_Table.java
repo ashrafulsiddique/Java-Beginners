e720e6ed06f722cb993b0752c6b7e0281f5bbc68
ashrafulsiddique <ashrafulsiddique@users.noreply.github.com>
2017-05-08 18:48:55 +0600

    Add files via upload

---
+++ Array_Table.java
@@ -0,0 +1,14 @@
+
+public class Array_Table {
+	public static void main(String[] args){
+		
+		System.out.println("Index\tValue");
+		int ash[]={32,13,45,2,54};
+		
+		
+		for(int counter=0;counter<ash.length;counter++){
+			System.out.println(counter + "\t" + ash[counter]);
+			
+		}
+	}
+}
