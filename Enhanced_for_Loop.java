ead01a1dfd5b9bdd0ea5dac04817323ff8a3c2bf
ashrafulsiddique <ashrafulsiddique@users.noreply.github.com>
2017-05-08 19:56:22 +0600

    Add files via upload

---
+++ Enhanced_for_Loop.java
@@ -0,0 +1,17 @@
+
+class Enhanced_for_Loop {
+	public static void main(String[] args){
+		int ashra[]={3,4,6,7,8};
+		int total=0;
+		
+		for(int x: ashra){
+			total+=x;
+			
+		}
+	System.out.println(total);
+	}
+	
+	
+	
+	
+}
